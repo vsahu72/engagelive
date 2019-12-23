983b34511c38a18a7c50e99f0d2a30434919d874
vikash sahu <vikash.aripra@gmail.com>
2019-12-23 13:42:29 +0530

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/Pods-engagelive_Example/Pods-engagelive_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_engagelive_Example : NSObject
+@end
+@implementation PodsDummy_Pods_engagelive_Example
+@end
---
+++ Example/Pods/Target Support Files/engagelive/engagelive-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_engagelive : NSObject
+@end
+@implementation PodsDummy_engagelive
+@end
